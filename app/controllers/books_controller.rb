d40748e28b5001921d3b57ad8c8bce298a63e363
吉田　慎一郎 <shinichiroyoshida@yoshidashinichirounoMacBook-Pro.local>
2022-05-05 22:30:33 +0900

    Third Edit 5/5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,6 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @newbook = Book.new
-    @user = @book.use
     @book_comment = BookComment.new
   end
 
